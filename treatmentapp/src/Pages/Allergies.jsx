b665ad7bbba4e3693f8636fb712b34b2be1198d3
Arik Amir <aamir@liveperson.com>
2023-08-09 05:37:22 +0000

    value={allergy.name}

--- treatmentapp/src/Pages/Allergies.jsx
+++ treatmentapp/src/Pages/Allergies.jsx
@@ -72,7 +72,7 @@ export default function Allergies() {
             <span className="checkbox-image">
                 <input
                     type="checkbox"
-                    value={allergy.value}
+                    value={allergy.name}
                     onChange={handleCheckboxChange}
                 />
             </span>
