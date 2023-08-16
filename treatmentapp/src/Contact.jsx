51ea29222382e32502ca136bd798465fb58626f2
dolevNagar <domlevnagar@gmail.com>
2023-08-16 15:17:02 +0300

    Zoom

--- treatmentapp/src/Contact.jsx
+++ treatmentapp/src/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-        <h1>ליצירת קשר ניתן לפנות במייל :  info@pimea-ai.com או בנייד 050-3333333</h1>
+        <h6>ליצירת קשר ניתן לפנות במייל :  info@pimea-ai.com או בנייד 050-3333333</h6>
     </div>
   )
 }
